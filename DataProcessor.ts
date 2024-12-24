85102d7883a50b00ce64215f2898727e588fd1e8
Nimira43 <n.rabone72@aol.com>
2024-12-24 02:32:01 +0000

    Created two methods inside the class - one to privately process the data, and the other a public method to get the processed data. Included an data of made up data, created an instance of DataProcessor. Lastly called the getProcessedData method to retrieve the processed data

--- DataProcessor.ts
+++ DataProcessor.ts
@@ -7,5 +7,17 @@ class DataProcessor {
     this.processedData = this.processData()
   }
 
- 
+  public getProcessedData(): number[] {
+    return this.processedData
+  }
+
+  private processData(): number[] {
+    return this.rawData.map(num => (num - Math.min(...this.rawData)))
+  }
 }
+
+const rawData = [45.959, 43.272, 52.226, 48.185, 46.195, 44.927]
+
+const processor = new DataProcessor(rawData)
+
+console.log('Processed Data: ', processor.getProcessedData())
